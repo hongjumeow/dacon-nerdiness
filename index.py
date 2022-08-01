760b2edf69a82feff5f396d193cec18c8bf886d7
Hongju Meow <hongjumeow@github.com>
2022-08-01 18:09:19 +0900

    add Random Forest Regressor

--- index.py
+++ index.py
@@ -6,6 +6,7 @@ warnings.filterwarnings('ignore')
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score
 
 def LabelEncodeData(data):
     label_encoder = LabelEncoder()
@@ -31,16 +32,27 @@ shape = (68, 15000)
 data = np.array(train_x)
 actual_nerdiness = np.array(train_y)
 
+
 # print(data[:, 26]) # see country labels 
 print(len(data[0]))
 
-d = []
-for i in range(len(index)):
-    if train_y[i] == 1:
-        d.append(data[:, 0][i])
 
-nd = np.array(d)
-
-print(nd.mean())
 
 train_features, test_features, train_labels, test_labels = train_test_split(data, train_y, test_size=0.25)
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+train_x_imputed = imputer.fit(train_features)
+train_x_imputed = imputer.transform(train_features)
+test_x_imputed = imputer.transform(test_features)
+
+regr = RandomForestRegressor()
+regr.fit(train_x_imputed, train_labels)
+
+y_pred = regr.predict(test_x_imputed)
+
+y_pred_bi = np.array([0 if i <= .5 else 1 for i in y_pred])
+print(y_pred_bi)
+
+acc_score = accuracy_score(y_pred_bi, test_labels)
+print(acc_score)
