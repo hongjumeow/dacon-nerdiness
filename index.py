437ef12876c23096e95c23219cc28476f121af02
Hongju Meow <hongjumeow@github.com>
2022-08-01 17:42:00 +0900

    studying

--- index.py
+++ index.py
@@ -25,8 +25,22 @@ train_x = train.drop(['nerdiness'], axis=1)
 train_x = LabelEncodeData(train_x)
 train_y = train['nerdiness']
 
+index = train_x.head(0).columns
+
 shape = (68, 15000)
 data = np.array(train_x)
 actual_nerdiness = np.array(train_y)
+
+# print(data[:, 26]) # see country labels 
+print(len(data[0]))
+
+d = []
+for i in range(len(index)):
+    if train_y[i] == 1:
+        d.append(data[:, 0][i])
+
+nd = np.array(d)
+
+print(nd.mean())
 
 train_features, test_features, train_labels, test_labels = train_test_split(data, train_y, test_size=0.25)
