049889198916996365c388609322b954fd7e6c2f
Hongju Meow <hongjumeow@github.com>
2022-08-01 12:03:50 +0900

    initialize data

--- index.py
+++ index.py
@@ -1,4 +1,3 @@
-from tkinter import Label
 import numpy as np
 import pandas as pd
 import warnings
@@ -15,22 +14,19 @@ def LabelEncodeData(data):
     data['country'] = label_encoder.fit_transform(data['country'])
 
     return data
-    
+
 train = pd.read_csv('competition-data/train.csv')
 test = pd.read_csv('competition-data/test.csv')
 
-train = train.drop(['index', 'country'], axis=1)
-test = test.drop(['index', 'country'], axis=1)
+train = train.drop(['index'], axis=1)
+test = test.drop(['index'], axis=1)
 
 train_x = train.drop(['nerdiness'], axis=1)
+train_x = LabelEncodeData(train_x)
 train_y = train['nerdiness']
 
 shape = (68, 15000)
 data = np.array(train_x)
 actual_nerdiness = np.array(train_y)
 
-
-data = LabelEncodeData(data)
-train_features, test_features, train_labels, test_labels = train_test_split(data, train_y, test_size=0.25, random_state=42)
-
-rf = RandomForestRegressor(n_estimators=15000, random_state=)
+train_features, test_features, train_labels, test_labels = train_test_split(data, train_y, test_size=0.25)
